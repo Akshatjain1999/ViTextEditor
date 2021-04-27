a40527020cb812ff529a1567d217539e5a9a8c4b
Akshat jain <43111888+Akshatjain1999@users.noreply.github.com>
2021-04-27 11:17:53 +0530

    Add files via upload

---
+++ TestEditor.UnitTests/BusinessLayerTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViTextEditor.BusinessLayer;
+using ViTextEditor.ClientLayer;
+using ViTextEditor.DataLayer;
+
+namespace TestEditor.UnitTests
+{
+    [TestClass]
+    public class BusinessLayerTest
+    {
+        [TestMethod]
+        public void IsExtentionTest()
+        {
+            //Arrange
+            var case1 = "Test";
+            var case2 = "test.txt";
+
+            //Act
+            var predictedTest1 = FileValidation.IsExtention(case1);
+            var predictedTest2 = FileValidation.IsExtention(case2);
+
+            //Assert
+            Assert.IsFalse(predictedTest1);
+            Assert.IsTrue(predictedTest2);
+
+        }
+
+        [TestMethod]
+        public void IsDirectoryTest()
+        {
+            var case1 = "C:\\Users\\akshat.jain\\Documents\\Visual Studio 2015\\Projects\\TextEditor\\TextEditor";
+            var case2 = "C:\\Users\\akshat.jain\\Documents\\Visual Studio 2015\\Projects\\TextEditor\\TextEdito";
+
+            //Act
+            var predictedTest1 = FileValidation.IsDirectory(case1);
+            var predictedTest2 = FileValidation.IsDirectory(case2);
+
+            Assert.IsTrue(predictedTest1);
+            Assert.IsFalse(predictedTest2);
+            
+        }
+
+        [TestMethod]
+        public void IsFileExistTest()
+        {
+            var case1 = @"C:\Users\akshat.jain\Documents\Visual Studio 2015\Projects\TextEditor\TextEditor\hello.txt";
+            var case2 = "C:\\Users\\akshat.jain\\Documents\\Visual Studio 2015\\Projects\\TextEditor\\testFile.txt";
+
+            //Act
+            var predictedTest1 = FileValidation.IsFileExits(case1);
+            var predictedTest2 = FileValidation.IsFileExits(case2);
+
+            Assert.IsTrue(predictedTest1);
+            Assert.IsFalse(predictedTest2);
+
+        }
+    }
+}
---
+++ TestEditor.UnitTests/DataLayerTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViTextEditor.ClientLayer;
+using ViTextEditor.DataLayer;
+
+namespace TestEditor.UnitTests
+{
+    [TestClass]
+    public class DataLayerTest
+    {
+        [TestMethod]
+        public void SaveNewFileTest()
+        {
+            //Arrange
+
+            var editor = new Editor()
+            {
+                CompletePath = @"C:\\Users\\akshat.jain\\Documents\\Visual Studio 2015\\Projects\\TextEditor\\TextEditor\\TestFile.txt",
+                FileContent = new StringBuilder("Testing a function")
+            };
+            var saveFile = new SaveFile();
+
+            //Act
+
+            saveFile.SaveNewFile(editor);
+
+            //Assert
+
+            Assert.IsTrue(File.Exists(editor.CompletePath));
+        }
+        
+
+        [TestMethod]
+        public void ReadFileExceptionTest()
+        {
+            var editor = new Editor()
+            {
+                CompletePath = @"C:\\Users\\akshat.jain\\Documents\\Visual Studio 2015\\Projects\\TextEditor\\TextEditor\\yello.txt",
+                FileContent = new StringBuilder("Testing a function")
+            };
+            try
+            {
+                var readFile = new ReadFile();
+                readFile.Read(editor);
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "File Does Not Exist.");
+                return;
+            }
+            Assert.Fail("No Exception Thrown");
+            
+        }
+    }
+}
